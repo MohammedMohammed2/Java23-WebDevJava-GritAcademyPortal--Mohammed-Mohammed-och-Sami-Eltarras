b7de1214f1a546878bb3edfe4dc3fdc9534fa507
MohammedMohammed2 <mohammed.mohammed1@gritacademy.se>
2024-02-16 16:20:23 +0100

    prgress!!

--- src/main/java/Servlet/CoursesServlet.java
+++ src/main/java/Servlet/CoursesServlet.java
@@ -1,17 +1,28 @@
 package Servlet;
 
+import org.example.JspUppgift.models.MysqlConnector;
+import org.example.JspUppgift.models.Userbean;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.LinkedList;
 
 @WebServlet(urlPatterns = "/Courses")
 public class CoursesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/jsp/courses.jsp").forward(req,resp);
 
+        LinkedList data = MysqlConnector.getConnector().selectQuery("allFromkurser");
+        Userbean usersBean = new Userbean();
+        usersBean.setData(data);
+
+        req.getSession().setAttribute("usersBean", usersBean);
+        System.out.println(((Userbean)(req.getSession().getAttribute("usersBean"))).getData());
+        req.getRequestDispatcher("/jsp/courses.jsp").forward(req, resp);
     }
 }
+
---
+++ src/main/webapp/jsp/courses.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Start Page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>Hello World from Show Students!</h1>
+        <h3>${param.message}</h3>
+    <table>
+
+        <c:forEach items="${usersBean.data }" var="dataPunkt">
+
+            <tr>
+                <td>${dataPunkt[1]}</td><td>${ dataPunkt[2] }</td><td>${ dataPunkt[3] }</td>
+            </tr>
+        </c:forEach>
+
+
+    </table>
+
+    </body>
+</html>
