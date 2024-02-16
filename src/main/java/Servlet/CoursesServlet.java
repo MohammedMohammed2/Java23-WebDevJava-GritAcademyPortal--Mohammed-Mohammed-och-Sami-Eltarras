2600fe99cf801f023b6cd85b1030f730f241887c
sami-elt <139684759+sami-elt@users.noreply.github.com>
2024-02-16 15:17:41 +0100

    .

--- src/main/java/Servlet/CoursesServlet.java
+++ src/main/java/Servlet/CoursesServlet.java
@@ -12,5 +12,6 @@ public class CoursesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/jsp/courses.jsp").forward(req,resp);
+
     }
 }
