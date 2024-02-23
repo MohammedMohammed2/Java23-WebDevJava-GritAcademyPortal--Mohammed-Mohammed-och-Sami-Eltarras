1f13fea525e2f57c445b3909db05ce77046b182e
sami-elt <139684759+sami-elt@users.noreply.github.com>
2024-02-23 14:24:38 +0100

    .

--- src/main/java/Servlet/ShowCourseServlet.java
+++ src/main/java/Servlet/ShowCourseServlet.java
@@ -37,7 +37,7 @@ public class ShowCourseServlet extends HttpServlet {
             }else {
                 searchStudent = dataOfSearch;
             }
-            
+
 
             req.setAttribute("searchAStudent", searchStudent);
             req.setAttribute("dataOfSearch", dataOfSearch);
